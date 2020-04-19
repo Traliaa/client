d53159f39e5557e41c3636b6ff2998fafeacb352
Tralai <a.a.korastelev@gmail.com>
2020-04-19 18:43:27 +0300

    add main

--- main.go
+++ main.go
@@ -2,43 +2,57 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 	"go.bug.st/serial.v1"
-	"log"
 	"net/url"
 	"os"
 	"os/signal"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:80", "http service address")
+type smartDevice struct {
+	IdDevice int
+	IdIot    int
+	Name     string
+	Status   bool
+}
+
+type Websoket_data struct {
+	Id_device   int
+	Device_name string
+	Divice_info string
+}
+
+type AuthDevice struct {
+	Username  string
+	Email     string
+	Processor string
+}
+
+var addr = flag.String("addr", "95.31.37.182:80", "http service address")
 
 func main() {
-	ports, err := serial.GetPortsList()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
-	}
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
+
+	m := smartDevice{
+		IdDevice: 001,
+		IdIot:    1,
+		Name:     "Led",
+		Status:   true,
 	}
 
+	findPorts()
 	flag.Parse()
-	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
-	log.Printf("connecting to %s", u.String())
+	logrus.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		logrus.Fatal("dial:", err)
 	}
 	defer c.Close()
 
@@ -47,12 +61,13 @@ func main() {
 	go func() {
 		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
+			err := c.ReadJSON(&m)
 			if err != nil {
-				log.Println("read:", err)
-				return
+				logrus.Println("Error reading json.", err)
 			}
-			log.Printf("recv: %s", message)
+
+			logrus.Printf("Got message: %#v\n", m)
+
 		}
 	}()
 
@@ -64,19 +79,20 @@ func main() {
 		case <-done:
 			return
 		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+
+			err = c.WriteJSON(&m)
 			if err != nil {
-				log.Println("write:", err)
-				return
+				logrus.Println("Error reading json.", err)
 			}
+			logrus.Println(t)
 		case <-interrupt:
-			log.Println("interrupt")
+			logrus.Println("interrupt")
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
-				log.Println("write close:", err)
+				logrus.Println("write close:", err)
 				return
 			}
 			select {
@@ -86,4 +102,18 @@ func main() {
 			return
 		}
 	}
+}
+
+func findPorts() {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	if len(ports) == 0 {
+		logrus.Fatal("No serial ports found!")
+	}
+	for _, port := range ports {
+		logrus.Printf("Found port: %v\n", port)
+	}
+
 }
